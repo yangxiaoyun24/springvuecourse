388916d46b9a5f52dabb46ee98fa85bd33733f0e
yangxiaoyun24 <1459949894@qq.com>
2020-05-14 19:46:51 +0800

    springcloud+vue 初始化项目

---
+++ src/test/java/com/imooc/springvuecourse/SpringvuecourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imooc.springvuecourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringvuecourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
