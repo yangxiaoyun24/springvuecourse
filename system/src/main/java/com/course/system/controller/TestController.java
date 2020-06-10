2372fbd45fbff412c3f914fdcfbaf34601994ab7
yangxiaoyun24 <1459949894@qq.com>
2020-06-10 19:37:29 +0800

    1、Example使用示例 使用Example可以快速完成单表的增删改查，熟练掌握后对代码开发效率将有一个质的飞跃

--- system/src/main/java/com/course/system/controller/TestController.java
+++ system/src/main/java/com/course/system/controller/TestController.java
@@ -1,7 +1,12 @@
 package com.course.system.controller;
 
+import com.course.server.domain.Test;
+import com.course.server.service.TestService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 public class TestController {
@@ -12,7 +17,8 @@ public class TestController {
 
 
     @RequestMapping("/test")
-    public String test(){
-        return "success";
+    public List<Test> test(){
+
+        return testService.list();
     }
 }
