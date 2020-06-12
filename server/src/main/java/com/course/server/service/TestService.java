db88ee3648081bbc4e07a85fff5ad160724d8df8
yangxiaoyun24 <1459949894@qq.com>
2020-06-12 17:12:38 +0800

    1、mybatis-generate自动生成代码，补充TestService

--- server/src/main/java/com/course/server/service/TestService.java
+++ server/src/main/java/com/course/server/service/TestService.java
@@ -1,6 +1,7 @@
 package com.course.server.service;
 
 import com.course.server.domain.Test;
+import com.course.server.domain.TestExample;
 import com.course.server.mapper.TestMapper;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +16,9 @@ public class TestService {
 
     public List<Test> list(){
 
-        return testMapper.list();
+        TestExample testExample = new TestExample();
+        testExample.createCriteria().andIdEqualTo("1");
+        testExample.setOrderByClause("id desc");
+        return testMapper.selectByExample(testExample);
     }
 }
