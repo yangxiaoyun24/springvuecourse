9c2decce54e997c00f4e3fbe3c3911b100fa70a9
yangxiaoyun24 <1459949894@qq.com>
2020-07-04 17:56:05 +0800

    1、路由修改，去掉了amdin

--- admin/src/router.js
+++ admin/src/router.js
@@ -1,31 +1,34 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Login from './views/login.vue'
-import Admin from './views/admin.vue'
-import Welcome from './views/admin/welcome.vue'
-import Chapter from './views/admin/chapter.vue'
+import Vue from "vue"
+import Router from "vue-router"
+import Login from "./views/login.vue"
+import Admin from "./views/admin.vue"
+import Welcome from "./views/admin/welcome.vue"
+import Chapter from "./views/admin/chapter.vue"
 
 
 
 Vue.use(Router);
 
 export default new Router({
-        mode:'history',
+        mode:"history",
     base:process.env.BASE_URL,
     routes:[{
-        path:'*',
+        path:"*",
         redirect:"/login",
     },{
-        path:'/login',
+        path:"/login",
         component:Login
     },{
-      path:'/admin',
+      path:"/",
+      name:"admin",
       component:Admin,
       children:[{
-        path:'welcome',
+        path:"welcome",
+        name:"admin",
         component:Welcome
       },{
-        path:'business/chapter',
+        path:"business/chapter",
+        name:"admin",
         component:Chapter
       }]
     }]
