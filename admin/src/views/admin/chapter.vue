45491f4c207467e81718b70be8fdf26e343ceb25
yangxiaoyun24 <1459949894@qq.com>
2020-07-04 17:40:07 +0800

    1、点击sidebar菜单实现页面跳转并增加激活样式

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -973,3 +973,14 @@
       </table>
 
 </template>
+
+<script>
+  export default {
+    name: "chapter",
+    mounted:function(){
+      this.$parent.activeSidebar("business-chapter-sidebar");
+    },
+    methods:{
+    }
+  }
+</script>
