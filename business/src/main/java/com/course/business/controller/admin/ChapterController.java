eb498d7112e9860cadb2a6a63bf25957c5255bff
yangxiaoyun24 <1459949894@qq.com>
2020-08-10 16:15:17 +0800

    1、分页参数前后端交互，axios的post请求默认是以流的方式传递参数，所以controller里的参数要加@RequestBody注解

--- business/src/main/java/com/course/business/controller/admin/ChapterController.java
+++ business/src/main/java/com/course/business/controller/admin/ChapterController.java
@@ -2,7 +2,11 @@ package com.course.business.controller.admin;
 
 import com.course.server.domain.Chapter;
 import com.course.server.dto.ChapterDto;
+import com.course.server.dto.PageDto;
 import com.course.server.service.ChapterService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,14 +17,17 @@ import java.util.List;
 @RequestMapping("/admin/chapter")
 public class ChapterController {
 
+    private static final Logger Log = LoggerFactory.getLogger(ChapterController.class);
+
 
     @Resource
     private ChapterService chapterService;
 
 
     @RequestMapping("/list")
-    public List<ChapterDto> list(){
-
-        return chapterService.list();
+    public PageDto list(@RequestBody PageDto pageDto){
+        Log.info("pageDto：" + pageDto);
+        chapterService.list(pageDto);
+        return pageDto;
     }
 }
