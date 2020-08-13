7f89b40aacb43eaaf81e7dd41280aac349061eea
yangxiaoyun24 <1459949894@qq.com>
2020-08-13 16:38:25 +0800

    1、增加修改大章功能，新增和修改用同一个保存功能，通过传入的参数id有没有值来判断

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -34,20 +34,11 @@
 
           <td>
             <div class="hidden-sm hidden-xs btn-group">
-              <button class="btn btn-xs btn-success">
-                <i class="ace-icon fa fa-check bigger-120"></i>
-              </button>
-
-              <button class="btn btn-xs btn-info">
+              <button v-on:click="edit(chapter)" class="btn btn-xs btn-info">
                 <i class="ace-icon fa fa-pencil bigger-120"></i>
               </button>
-
               <button class="btn btn-xs btn-danger">
                 <i class="ace-icon fa fa-trash-o bigger-120"></i>
-              </button>
-
-              <button class="btn btn-xs btn-warning">
-                <i class="ace-icon fa fa-flag bigger-120"></i>
               </button>
             </div>
 
@@ -144,6 +135,13 @@
     methods:{
       add(){
         let _this = this;
+        _this.chapter = {};
+        $("#form-modal").modal("show");
+      },
+
+      edit(chapter){
+        let _this = this;
+        _this.chapter = $.extend({},chapter);
         $("#form-modal").modal("show");
       },
 
