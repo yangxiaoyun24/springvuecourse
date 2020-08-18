4ac14305a687320e675eae8638be8f61da6920b5
yangxiaoyun24 <1459949894@qq.com>
2020-08-18 11:08:54 +0800

    1、生成器开发：增加生成controller层的代码

--- generator/src/main/java/com/course/generator/server/ServerGenerator.java
+++ generator/src/main/java/com/course/generator/server/ServerGenerator.java
@@ -10,6 +10,7 @@ import java.util.Map;
 public class ServerGenerator {
 
     static String toServicePath = "server\\src\\main\\java\\com\\course\\server\\service\\";
+    static String toControllerPath = "business\\src\\main\\java\\com\\course\\business\\controller\\admin\\";
 
     public static void main(String[] args) throws IOException, TemplateException {
 
@@ -19,7 +20,12 @@ public class ServerGenerator {
         map.put("Domain",Domain);
         map.put("domain",domain);
 
+        //生成service
         FreemarkerUtil.initConfig("service.ftl");
-        FreemarkerUtil.generator(toServicePath + Domain + "Service.java",map);
+        FreemarkerUtil.generator(toServicePath + Domain + "Service.java",map);        //生成service
+
+        //生成controller
+        FreemarkerUtil.initConfig("controller.ftl");
+        FreemarkerUtil.generator(toControllerPath + Domain + "Controller.java",map);
     }
 }
