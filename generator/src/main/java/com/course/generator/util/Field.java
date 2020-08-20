18336c665884b6d9cc833771356187018eb7d259
yangxiaoyun24 <1459949894@qq.com>
2020-08-20 11:17:33 +0800

    1、生成器开发：增加dto生成

---
+++ generator/src/main/java/com/course/generator/util/Field.java
@@ -0,0 +1,88 @@
+package com.course.generator.util;
+
+/**
+ * Field类，用于存储每个字段的信息。
+ * 流程：根据表名获取所有的字段信息，再将字段信息填充到Field类中，
+ * 得到Field列表，之后将Field列表变量传入模板
+ */
+public class Field {
+
+    private String name;     //字段名：course_id
+    private String nameHump; //字段名小驼峰：courseId
+    private String nameBigHump; //字段名大驼峰：CourseId
+    private String nameCn;   //中文名：课程
+    private String type;     //字段类型：char(8)
+    private String javaType; //java类型：String
+    private String comment;  //注释：课程/ID
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNameHump() {
+        return nameHump;
+    }
+
+    public void setNameHump(String nameHump) {
+        this.nameHump = nameHump;
+    }
+
+    public String getNameBigHump() {
+        return nameBigHump;
+    }
+
+    public void setNameBigHump(String nameBigHump) {
+        this.nameBigHump = nameBigHump;
+    }
+
+    public String getNameCn() {
+        return nameCn;
+    }
+
+    public void setNameCn(String nameCn) {
+        this.nameCn = nameCn;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getJavaType() {
+        return javaType;
+    }
+
+    public void setJavaType(String javaType) {
+        this.javaType = javaType;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    @Override
+    public String toString() {
+        return "Field{" +
+                "name='" + name + '\'' +
+                ", nameHump='" + nameHump + '\'' +
+                ", nameBigHump='" + nameBigHump + '\'' +
+                ", nameCn='" + nameCn + '\'' +
+                ", type='" + type + '\'' +
+                ", javaType='" + javaType + '\'' +
+                ", comment='" + comment + '\'' +
+                '}';
+    }
+
+}
