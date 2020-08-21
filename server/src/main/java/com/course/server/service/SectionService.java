b4ecf7746b0c61a7a7438fda4ffc3f5512814a66
yangxiaoyun24 <1459949894@qq.com>
2020-08-21 16:59:38 +0800

    1、生成器开发，通用字段的处理，id,createdAt,updatedAt,sort

--- server/src/main/java/com/course/server/service/SectionService.java
+++ server/src/main/java/com/course/server/service/SectionService.java
@@ -14,6 +14,7 @@ import org.springframework.util.StringUtils;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.Date;
 
 @Service
 public class SectionService {
@@ -29,6 +30,7 @@ public class SectionService {
     public void list(PageDto pageDto){
         PageHelper.startPage(pageDto.getPage(),pageDto.getSize());
         SectionExample sectionExample = new SectionExample();
+        sectionExample.setOrderByClause("sort asc");
         List<Section> sectionList = sectionMapper.selectByExample(sectionExample);
         PageInfo<Section> pageInfo = new PageInfo<>(sectionList);
         pageDto.setTotal(pageInfo.getTotal());
@@ -55,6 +57,9 @@ public class SectionService {
      * @param section
      */
     private void insert(Section section) {
+        Date now = new Date();
+        section.setCreatedAt(now);
+        section.setUpdatedAt(now);
         section.setId(UuidUtil.getShortUuid());
         sectionMapper.insert(section);
     }
@@ -64,6 +69,7 @@ public class SectionService {
      * @param section
      */
     private void update(Section section) {
+        section.setUpdatedAt(new Date());
         sectionMapper.updateByPrimaryKey(section);
     }
 
