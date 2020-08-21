6d58001e46fd92f5798dafb34c5f2c8e23bf9fe5
yangxiaoyun24 <1459949894@qq.com>
2020-08-21 15:21:38 +0800

    1、生成器开发，增加生成vue界面代码 2、小节管理增删改查功能测试成功

--- admin/src/router.js
+++ admin/src/router.js
@@ -4,6 +4,7 @@ import Login from "./views/login.vue"
 import Admin from "./views/admin.vue"
 import Welcome from "./views/admin/welcome.vue"
 import Chapter from "./views/admin/chapter.vue"
+import Section from "./views/admin/section.vue"
 
 
 
@@ -25,11 +26,15 @@ export default new Router({
       children:[{
         path:"welcome",
         name:"welcome",
-        component:Welcome
+        component:Welcome,
       },{
         path:"business/chapter",
         name:"business/chapter",
-        component:Chapter
+        component:Chapter,
+      },{
+        path:"business/section",
+        name:"business/section",
+        component:Section,
       }]
     }]
 })
