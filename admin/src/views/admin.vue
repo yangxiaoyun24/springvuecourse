eb7a1d5d8fbdd3a78a6e3fe99ca9b79bbe168a1f
yangxiaoyun24 <1459949894@qq.com>
2020-11-04 15:48:14 +0800

    1、增加讲师管理功能

--- admin/src/views/admin.vue
+++ admin/src/views/admin.vue
@@ -408,14 +408,22 @@
                   <i class="menu-icon fa fa-caret-right"></i>
                   分类管理
                 </router-link>
-
                 <b class="arrow"></b>
-              </li><li class="active" id="business-course-sidebar">
+              </li>
+
+              <li class="active" id="business-course-sidebar">
                 <router-link to="/business/course">
                   <i class="menu-icon fa fa-caret-right"></i>
                   课程管理
                 </router-link>
+                <b class="arrow"></b>
+              </li>
 
+              <li class="active" id="business-teacher-sidebar">
+                <router-link to="/business/teacher">
+                  <i class="menu-icon fa fa-caret-right"></i>
+                  讲师管理
+                </router-link>
                 <b class="arrow"></b>
               </li>
               <!--<li class="active" id="business-chapter-sidebar">-->
