c6b42ccf12948a9ff585be66475b3de8143e6c61
yangxiaoyun24 <1459949894@qq.com>
2020-11-04 16:20:40 +0800

    1、课程列表中显示讲师信息 2、课程名称字体自适应

--- admin/src/views/admin/course.vue
+++ admin/src/views/admin/course.vue
@@ -36,6 +36,17 @@
             <h3 class="search-title">
               <a href="#" class="blue">{{course.name}}</a>
             </h3>
+
+            <div v-for="teacher in teachers.filter(t=>{return t.id===course.teacherId})" class="profile-activity clearfix">
+              <div>
+                <img v-show="!teacher.image" class="pull-left" src="/ace/assets/images/avatars/avatar5.png">
+                <img v-show="teacher.image" class="pull-left" v-bind:src="teacher.image">
+                <a class="user" href="#"> {{teacher.name}} </a>
+                <br>
+                {{teacher.position}}
+              </div>
+            </div>
+
             <p>
               <span class="blue bolder bigger-150">{{course.price}}&nbsp;<i class="fa fa-rmb"></i></span>&nbsp;
             </p>
@@ -43,7 +54,7 @@
             <p>
               <span class="badge badge-info">{{course.id}}</span>
               <span class="badge badge-info">排序:{{course.sort}}</span>
-              <span class="badge badge-info">时长:{{course.time | formatSecond}}</span>
+              <span class="badge badge-info">{{course.time | formatSecond}}</span>
             </p>
             <p>
               <button v-on:click="toChapter(course)" class="btn btn-white btn-xs btn-info btn-round">
@@ -561,5 +572,11 @@
 <style scoped>
   .caption h3{
     font-size: 20px;
+  }
+  /*字体大小自适应，根据屏幕的分辨率显示不同大小的字体*/
+  @media (max-width: 1199px) {
+    .caption h3 {
+      font-size: 16px;
+    }
   }
 </style>
