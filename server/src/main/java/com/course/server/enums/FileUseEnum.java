4d7bd0aaab2a72fd9a8d4df11e77e0f18c4cd4cc
yangxiaoyun24 <1459949894@qq.com>
2020-11-06 18:46:16 +0800

    1、文件上传功能开发：文件表设计与基本代码生成

---
+++ server/src/main/java/com/course/server/enums/FileUseEnum.java
@@ -0,0 +1,34 @@
+package com.course.server.enums;
+
+public enum FileUseEnum {
+
+    COURSE("C","讲师"),
+    TEACHER("T","课程");
+
+    private String code;
+
+    private String desc;
+
+    FileUseEnum(String code,String desc){
+        this.code = code;
+        this.desc = desc;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+
+}
