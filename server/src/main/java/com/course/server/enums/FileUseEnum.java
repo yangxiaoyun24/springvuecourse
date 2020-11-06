7ad456c7ce59a7c79a205da92959403f8a0ec211
yangxiaoyun24 <1459949894@qq.com>
2020-11-06 22:22:55 +0800

    1、文件上传功能开发：文件按用途分类保存

--- server/src/main/java/com/course/server/enums/FileUseEnum.java
+++ server/src/main/java/com/course/server/enums/FileUseEnum.java
@@ -2,8 +2,8 @@ package com.course.server.enums;
 
 public enum FileUseEnum {
 
-    COURSE("C","讲师"),
-    TEACHER("T","课程");
+    COURSE("C","课程"),
+    TEACHER("T","讲师");
 
     private String code;
 
@@ -28,6 +28,15 @@ public enum FileUseEnum {
 
     public void setDesc(String desc) {
         this.desc = desc;
+    }
+
+    public static FileUseEnum getByCode(String code){
+        for(FileUseEnum e: FileUseEnum.values()){
+            if(code.equals(e.getCode())){
+                return e;
+            }
+        }
+        return null;
     }
 
 
